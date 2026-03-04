5585561d72217e0023d399a02ae53ed5b8c2934c
heathersherry <heathersherrylin@gmail.com>
2026-03-04 15:08:48 +0800

    refactor views

--- muller/core/view/__init__.py
+++ muller/core/view/__init__.py
@@ -6,4 +6,4 @@
 #
 # Copyright (c) 2026 Bingyu Liu
 
-from .interface.dataset_interface_view import get_views, get_view, save_view, load_view, delete_view, get_view_for_vds
+from .view_operations import get_views, get_view, save_view, load_view, delete_view, get_view_for_vds
