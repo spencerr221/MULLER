53f02036b7acdc3fe8bc0a402fc4cc83f3fb2944
heathersherry <heathersherrylin@gmail.com>
2026-03-10 16:21:39 +0800

    modify file header

--- muller/api/dataset/core.py
+++ muller/api/dataset/core.py
@@ -4,6 +4,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
+# This file was originally part of Hub (now Deep Lake) project: https://github.com/activeloopai/deeplake/tree/release/2.8.5
+# Commit: https://github.com/activeloopai/deeplake/tree/94c5e100292c164b80132baf741ef233dd41f3d7
+# Source: https://github.com/activeloopai/deeplake/blob/94c5e100292c164b80132baf741ef233dd41f3d7/hub/api/dataset.py
+#
 # Modifications Copyright (c) 2026 Xueling Lin
 
 """Core dataset operations: create, load, empty, delete, get_col_info."""
--- muller/api/info.py
+++ muller/api/info.py
@@ -4,11 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# This file was originally part of Hub (now Deep Lake) project: https://github.com/activeloopai/deeplake/tree/release/2.8.5
-# Commit: https://github.com/activeloopai/deeplake/tree/94c5e100292c164b80132baf741ef233dd41f3d7
-# Source: https://github.com/activeloopai/deeplake/blob/94c5e100292c164b80132baf741ef233dd41f3d7/hub/api/info.py
-#
-# Modifications Copyright (c) 2026 Xueling Lin
+# Copyright (c) 2026 Xueling Lin
 
 # Re-export Info from core.storage for backward compatibility
 from muller.core.storage.info import Info
