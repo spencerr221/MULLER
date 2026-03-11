49a13e918f5a3d11b8d77df264117747c74c46c0
heathersherry <heathersherrylin@gmail.com>
2026-03-11 11:07:29 +0800

    add python header

--- muller/core/query/safe_evaluator.py
+++ muller/core/query/safe_evaluator.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: MPL-2.0
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2026 Xueling Lin
+
 """
 Safe query evaluator using AST-based whitelist approach.
 
